05b7bd48c12c9301539c19d11ba2ce29ca1ff1d1
kfradd <ken@fradd.co.uk>
2021-07-08 21:02:57 +0100

    Initial commit

---
+++ src/main/java/com/example/springbootstarterdataneo4j/SpringBootStarterDataNeo4jApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootstarterdataneo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterDataNeo4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStarterDataNeo4jApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootstarterdataneo4j/SpringBootStarterDataNeo4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootstarterdataneo4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterDataNeo4jApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
